aa878e67bb9fa909374d94e56b484a99f03fd802
Gustavo Prevelate Ribeiro Da Silva <guprevelate2022@gmail.com>
2023-11-17 08:42:06 -0300

    Atualização de estrutura 2
    Atualizando cores do projeto

--- MobileTCCProliseum-main/ProliseumTCCV7/app/src/main/java/br/senai/sp/jandira/proliseumtcc/ui/theme/Color.kt
+++ MobileTCCProliseum-main/ProliseumTCCV7/app/src/main/java/br/senai/sp/jandira/proliseumtcc/ui/theme/Color.kt
@@ -10,9 +10,13 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-//Proliseum
+// CORES DA PLATAFORMA - PROLISEUM
 
+// VERMELHO
 val RedProliseum = Color(0xFFFF3130)
+
+// AZUL ESCURO
 val AzulEscuroProliseum = Color(0xFF1F1E31)
+
+// PRETO TRANSPARENTE
 val BlackTransparentProliseum = Color(0x33000000)
-val WhiteTransparentProliseum = Color(0x80FFFFFF)
